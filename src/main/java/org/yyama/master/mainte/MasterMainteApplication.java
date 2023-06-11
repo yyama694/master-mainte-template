c420b0b84ddc5fccc18a762d30d26fc28343121a
yyama694 <yyama694@gmail.com>
2023-06-11 14:27:38 +0900

    mybatis実装中

--- src/main/java/org/yyama/master/mainte/MasterMainteApplication.java
+++ src/main/java/org/yyama/master/mainte/MasterMainteApplication.java
@@ -2,8 +2,9 @@ package org.yyama.master.mainte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class MasterMainteApplication {
 
 	public static void main(String[] args) {
